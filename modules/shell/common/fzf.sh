a6ff8abb6cf84eb25d0cf1cb05ad3afae7fab627
FrauH0lle <roland.goers@posteo.de>
2024-01-13 21:10:11 +0100

    confgen: Implement simple template evalutation
    - Anything between '##%% [[ CONDITION ]]' and '##%%' will only be
      inserted if the condition is true

--- modules/shell/common/fzf.sh
+++ modules/shell/common/fzf.sh
@@ -1,3 +1,4 @@
+##%% [[ "$dd_distro" == "gentoo" ]]
 if [ "$IS_ZSH" = "true" ]; then
     source /usr/share/zsh/site-functions/_fzf
     source /usr/share/fzf/key-bindings.zsh
@@ -7,9 +8,35 @@ if [ "$IS_BASH" = "true" ]; then
     source /usr/share/bash-completion/completions/fzf
     source /usr/share/fzf/key-bindings.bash
 fi
+##%% end
+##%% [[ "$dd_distro" == "ubuntu" ]]
+if [ "$IS_ZSH" = "true" ]; then
+    source /usr/share/doc/fzf/examples/completion.zsh
+    source /usr/share/doc/fzf/examples/key-bindings.zsh
+fi
 
+if [ "$IS_BASH" = "true" ]; then
+    source /usr/share/doc/fzf/examples/key-bindings.bash
+fi
+##%% end
+
+##%% [[ "$dd_distro" == "gentoo" ]]
 export FZF_DEFAULT_COMMAND='fd -H -I'
+##%% end
+##%% [[ "$dd_distro" == "ubuntu" ]]
+export FZF_DEFAULT_COMMAND='fdfind -H -I'
+##%% end
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="--preview '([[ -f {} ]] && (bat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'"
+##%% [[ "$dd_distro" == "gentoo" ]]
 export FZF_ALT_C_COMMAND='fd -H -I --type d'
+##%% end
+##%% [[ "$dd_distro" == "ubuntu" ]]
+export FZF_ALT_C_COMMAND='fdfind -H -I --type d'
+##%% end
+##%% [[ "$dd_distro" == "gentoo" ]]
 export FZF_ALT_C_OPTS="--preview 'eza --long --tree --level=1 --color=auto --icons=auto --group-directories-first -- {}'"
+##%% end
+##%% [[ "$dd_distro" == "ubuntu" ]]
+export FZF_ALT_C_OPTS="--preview 'exa --long --tree --level=1 --color=auto --icons=auto --group-directories-first -- {}'"
+##%% end
